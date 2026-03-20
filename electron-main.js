a2f2f25c18d5ac4a0274b86f5f0d3917dd76f945
MISTER_NEGATIVE <supergogetavegito21@gmail.com>
2026-03-20 05:44:06 +0530

    dd

--- electron-main.js
+++ electron-main.js
@@ -20,23 +20,35 @@ function createWindow() {
   // Check if we are in development or production
   const isDev = process.env.NODE_ENV === 'development';
 
+  const PORT = 3077;
+
   if (isDev) {
-    win.loadURL('http://localhost:3000');
+    win.loadURL(`http://localhost:${PORT}`);
     win.webContents.openDevTools();
   } else {
     // In production, start the Next.js server
-    // Use the unpacked path for the next binary since asar binaries cannot be spawned
-    const nextPath = path.join(__dirname.replace('app.asar', 'app.asar.unpacked'), 'node_modules', '.bin', 'next');
+    // Use the unpacked path since asar contents cannot be executed directly
+    const appPath = app.getAppPath();
+    const unpackedPath = appPath.replace('app.asar', 'app.asar.unpacked');
     
-    nextProcess = spawn(nextPath, ['start', '-p', '3000'], {
-      cwd: __dirname.replace('app.asar', 'app.asar.unpacked'),
-      env: { ...process.env, NODE_ENV: 'production' }
+    // Point directly to the next CLI script
+    const nextPath = path.join(unpackedPath, 'node_modules', 'next', 'dist', 'bin', 'next');
+    
+    // Spawn using the current process execPath (which is Electron)
+    // and set ELECTRON_RUN_AS_NODE to run it as a regular Node.js script
+    nextProcess = spawn(process.execPath, [nextPath, 'start', '-p', PORT.toString()], {
+      cwd: unpackedPath,
+      env: { 
+        ...process.env, 
+        NODE_ENV: 'production',
+        ELECTRON_RUN_AS_NODE: '1'
+      }
     });
 
     // Give it a few seconds to start
     const pollServer = () => {
-      fetch('http://localhost:3000')
-        .then(() => win.loadURL('http://localhost:3000'))
+      fetch(`http://localhost:${PORT}`)
+        .then(() => win.loadURL(`http://localhost:${PORT}`))
         .catch(() => setTimeout(pollServer, 1000));
     };
     
